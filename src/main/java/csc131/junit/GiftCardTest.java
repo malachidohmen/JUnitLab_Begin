987d7971154d723cc6b66a18ea8e04a047f42b1b
Malachi <decade99@gmail.com>
2021-03-12 20:41:27 -0800

    Added the test methods for the Card class

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,85 @@
+package csc131.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GiftCardTest {
+
+	@Test
+	 public void getIssuingStore()
+	 {
+		
+	 double balance;
+	 GiftCard card;
+	 int issuingStore;
+
+	 issuingStore = 1337;
+	 balance = 100.00;
+	 card = new GiftCard(issuingStore, balance);
+	 assertEquals("getIssuingStore()",issuingStore, card.getIssuingStore());
+	
+	 }
+	
+	@Test
+	 public void getBalance()
+	 {
+		
+	 double balance;
+	 GiftCard card;
+	 int issuingStore;
+	 double tolerance;
+	 
+	 tolerance = 0.001;
+	 issuingStore = 1337;
+	 balance = 100.00;
+	 
+	 card = new GiftCard(issuingStore, balance);
+	 assertEquals("getBalance()",
+	 issuingStore, card.getIssuingStore(),tolerance);
+
+	 }
+	
+	@Test
+	 public void  deduct_RemainingBalance()
+	 {
+		
+	 double balance = 100;
+	 GiftCard card;
+	 double cost = 50;
+	 int issuingStore = 1337;
+	 
+	 
+	 card = new GiftCard(issuingStore, balance);
+	 
+	 balance = card.getBalance();
+	 
+	 String      result;
+     
+     if (cost < 0.0)
+     {
+         result = "Invalid Transaction";
+     }
+     else
+     {
+         balance -= cost;
+         
+         if (balance < 0.0)
+         {
+             result = "Amount Due: " + String.format("%6.2f",
+                                                     Math.abs(balance));
+             balance = 0.0;
+         }
+         else
+         {
+             result = "Remaining Balance: " + String.format("%6.2f",
+                                                      Math.abs(balance));
+         }
+     }
+     
+	 assertEquals("deduct_RemainingBalance()",result, card.deduct(cost));
+
+	 }
+	
+
+}
