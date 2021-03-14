2e63a91a207b85f0fa4e08a554fa69cd83c4043b
Malachi <decade99@gmail.com>
2021-03-14 12:34:24 -0700

    - Added final tests for GiftCard class

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -46,7 +46,7 @@ public class GiftCardTest {
 		
 	 double balance = 100;
 	 GiftCard card;
-	 double cost = 50;
+	 double cost = 0;
 	 int issuingStore = 1337;
 	 
 	 
@@ -55,31 +55,61 @@ public class GiftCardTest {
 	 balance = card.getBalance();
 	 
 	 String      result;
-     
-     if (cost < 0.0)
+     for(cost = -50; cost < 100; cost++)
      {
-         result = "Invalid Transaction";
-     }
-     else
-     {
-         balance -= cost;
-         
-         if (balance < 0.0)
-         {
-             result = "Amount Due: " + String.format("%6.2f",
-                                                     Math.abs(balance));
-             balance = 0.0;
-         }
-         else
-         {
-             result = "Remaining Balance: " + String.format("%6.2f",
-                                                      Math.abs(balance));
-         }
+	     if (cost < 0.0)
+	     {
+	         result = "Invalid Transaction";
+	     }
+	     else
+	     {
+	         balance -= cost;
+	         
+	         if (balance < 0.0)
+	         {
+	             result = "Amount Due: " + String.format("%6.2f",
+	                                                     Math.abs(balance));
+	             balance = 0.0;
+	         }
+	         else
+	         {
+	             result = "Remaining Balance: " + String.format("%6.2f",
+	                                                      Math.abs(balance));
+	         }
+	     }
+	     
+		 assertEquals("deduct_RemainingBalance()",result, card.deduct(cost));
+		 
      }
      
-	 assertEquals("deduct_RemainingBalance()",result, card.deduct(cost));
-
-	 }
+   }
 	
+	@Test
+	public void constructor_Incorrect_OpeningBalance_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	@Test
+    public void constructor_IncorrectID_Low() {
+        assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+  }
+	
+    @Test
+    public void constructor_IncorrectID_High() {
+        assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100.00);});
 
+    }
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
